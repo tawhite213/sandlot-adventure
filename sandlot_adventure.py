817665b9b33f697bb01ba434bb6b75f7e5054ba5
Tristan White <tawhite213@gmail.com>
2026-07-20 15:44:02 -0500

    Delete sandlot_adventure.py

--- sandlot_adventure.py
+++
@@ -1,9 +0,0 @@
-# Name: Tristan White
-# Date: 7/5/2026
-# Program Purpose: Display a "Hello World message".
-# Program is being compared to other programming languages.
-
-
-#Display the message to the user
-
-print("Hello, World!")
